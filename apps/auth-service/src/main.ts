90f1bfa15333586ca986a395bcef94534d81da7e
Maitrek Patel <maitrekpatel1612@gmail.com>
2025-08-29 22:53:33 +0530

    Configured API Gateway and Auth Service, Defined PORT Number of both 8080 and 6001 respectively

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {l
     res.send({ 'message': 'Hello API'});
 });
 
