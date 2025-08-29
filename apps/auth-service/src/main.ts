fc70518cd515bdc22fc6b221e31aa57d80ef959c
Maitrek Patel <maitrekpatel1612@gmail.com>
2025-08-30 00:36:48 +0530

    use errorMiddleware into auth service also set cookieParser and other Middlewares in same

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
+import { errorMiddleware
 
+ } from "../../../packages/error-handler/error-middleware";
+import cookieParser from "cookie-parser";
 //Create express app
 const app = express();
 
@@ -13,12 +16,16 @@ app.use(
   })
 );
 
+app.use(express.json());
+app.use(cookieParser());
 //Routes
 
 app.get("/", (req, res) => {
   res.send({ message: "Hello API from Auth-Service" });
 });
 
+//Error Middleware
+app.use(errorMiddleware);
 //Start server
 
 const port = process.env.PORT || 6001;
